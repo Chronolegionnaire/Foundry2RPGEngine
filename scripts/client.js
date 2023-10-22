9b46cba21a8a9d6f6cd41a224fd57944d19d6a63
OsianLinton <o.linton@hotmail.co.uk>
2023-10-22 01:46:01 +0100

    refactoring

--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,40 @@
-const url = 'http://localhost:8001/TheRpgEngine';
+const baseUrl = 'http://localhost:8001/TheRpgEngine';
+const headers = {
+    'Content-Type': 'application/json'
+};
 
-export async function postChat(message) {
-    var response = await fetch(`${url}/chat`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+async function post(url, message) {
+    return await fetch(url, {
+        method: "POST",
+        headers: headers,
         body: message,
         mode: 'no-cors'
-    })
+    });
+}
+
+async function get(url) {
+    return await fetch(url, {
+        method: "GET",
+        headers: headers,
+        mode: 'no-cors'
+    });
+}
+
+export async function postChat(message) {
+    let response = await get(`${baseUrl}/chat`, message);
 
     return JSON.parse(response.json);
+}
+
+export async function getFx() {
+    let response = await get(`${baseUrl}/fx`);
+
+    return JSON.parse(response.json);
+}
+
+export async function postFx(name, position) {
+    await post(`${baseUrl}/fx`, {
+        name: name,
+        position: position
+    });
 }
--- scripts/preCreateChatMessage.js
+++ scripts/preCreateChatMessage.js
@@ -2,7 +2,7 @@ import { postChat } from "./client";
 
 export async function process(msg) {
     if (msg.isRoll) {
-        for (let i = 0; i < msg.rolls.length; i++) {
+        for (var i = 0; i < msg.rolls.length; i++) {
             let formula = cleanFormula(msg.rolls[i]._formula);
             postChat(formula);
         }

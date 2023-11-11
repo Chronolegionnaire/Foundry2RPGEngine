25d9f77e3a2d242ed4fc1c3030a91c851ca130c4
OsianLinton <o.linton@hotmail.co.uk>
2023-11-11 00:21:00 +0000

    1.3 - Refactored and fixed manifest file links

--- foundry2rpgengine.js
+++ foundry2rpgengine.js
@@ -1,4 +1,4 @@
-import { process as preCreateChatMessage } from "./scripts/preCreateChatMessage";
+import { process as preCreateChatMessage } from "./preCreateChatMessage";
 
 Hooks.once("ready", () => {
     console.log("foundry2rpgengine | Initializing Foundry 2 Rpg Engine");
